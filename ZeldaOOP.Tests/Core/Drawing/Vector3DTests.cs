3e3de8cb03a26b8282c676da862b1573f0ab9a21
Jason Palacios <jason.palacios@utexas.edu>
2017-11-06 13:25:22 -0600

    Implemented some 3D vector operations. Some unit tests aren't passing due to precision of floating point values and checking for equality. Planning to use epsilon based comparison.

---
+++ ZeldaOOP.Tests/Core/Drawing/Vector3DTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ZeldaOOP.Core.Drawing;
+
+namespace ZeldaOOP.Tests.Core.Drawing
+{
+    [TestClass]
+    public class Vector3DTests
+    {
+        [TestMethod]
+        public void IJCrossProductK()
+        {
+            var i = new Vector3D<float>
+            {
+                X = 1.0f,
+                Y = 0.0f,
+                Z = 0.0f
+            };
+
+            var j = new Vector3D<float>
+            {
+                X = 0.0f,
+                Y = 1.0f,
+                Z = 0.0f
+            };
+
+            var expectedCrossProduct = new Vector3D<float>
+            {
+                X = 0.0f,
+                Y = 0.0f,
+                Z = 1.0f
+            };
+
+            var crossProduct = i.Cross(j);
+
+            Assert.IsTrue(crossProduct.SequenceEqual(expectedCrossProduct));
+        }
+
+        [TestMethod]
+        public void UnitVectorMagnitudeIsOne()
+        {
+            var unitVector = new Vector3D<float>
+            {
+                X = (float)(1.0f / Math.Sqrt(3.0f)),
+                Y = (float)(1.0f / Math.Sqrt(3.0f)),
+                Z = (float)(1.0f / Math.Sqrt(3.0f))
+            };
+
+            var expectedMagnitude = 1.0f;
+            var epsilon = float.Epsilon;
+
+            // TODO: Finish me! Numeric precision is a pain
+
+            var magnitude = unitVector.Magnitude();
+
+            Assert.AreEqual(expectedMagnitude, expectedMagnitude);
+        }
+    }
+}
